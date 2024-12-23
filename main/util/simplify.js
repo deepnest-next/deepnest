44e942ad85c3e981d4b7ae82d13eb6aff366b58a
Josef Fröhle <github@josef-froehle.de>
2024-12-23 15:51:32 +0100

    format all files

--- main/util/simplify.js
+++ main/util/simplify.js
@@ -5,100 +5,98 @@
  modified by Jack Qiao
 */
 
-(function () { 'use strict';
+(function () {
+  "use strict";
 
-// to suit your point format, run search/replace for '.x' and '.y';
-// for 3D version, see 3d branch (configurability would draw significant performance overhead)
+  // to suit your point format, run search/replace for '.x' and '.y';
+  // for 3D version, see 3d branch (configurability would draw significant performance overhead)
 
-// square distance between 2 points
-function getSqDist(p1, p2) {
-
+  // square distance between 2 points
+  function getSqDist(p1, p2) {
     var dx = p1.x - p2.x,
-        dy = p1.y - p2.y;
+      dy = p1.y - p2.y;
 
     return dx * dx + dy * dy;
-}
+  }
 
-// square distance from a point to a segment
-function getSqSegDist(p, p1, p2) {
-
+  // square distance from a point to a segment
+  function getSqSegDist(p, p1, p2) {
     var x = p1.x,
-        y = p1.y,
-        dx = p2.x - x,
-        dy = p2.y - y;
+      y = p1.y,
+      dx = p2.x - x,
+      dy = p2.y - y;
 
     if (dx !== 0 || dy !== 0) {
+      var t = ((p.x - x) * dx + (p.y - y) * dy) / (dx * dx + dy * dy);
 
-        var t = ((p.x - x) * dx + (p.y - y) * dy) / (dx * dx + dy * dy);
-
-        if (t > 1) {
-            x = p2.x;
-            y = p2.y;
-
-        } else if (t > 0) {
-            x += dx * t;
-            y += dy * t;
-        }
+      if (t > 1) {
+        x = p2.x;
+        y = p2.y;
+      } else if (t > 0) {
+        x += dx * t;
+        y += dy * t;
+      }
     }
 
     dx = p.x - x;
     dy = p.y - y;
 
     return dx * dx + dy * dy;
-}
-// rest of the code doesn't care about point format
+  }
+  // rest of the code doesn't care about point format
 
-// basic distance-based simplification
-function simplifyRadialDist(points, sqTolerance) {
-
+  // basic distance-based simplification
+  function simplifyRadialDist(points, sqTolerance) {
     var prevPoint = points[0],
-        newPoints = [prevPoint],
-        point;
+      newPoints = [prevPoint],
+      point;
 
     for (var i = 1, len = points.length; i < len; i++) {
-        point = points[i];
+      point = points[i];
 
-        if (point.marked || getSqDist(point, prevPoint) > sqTolerance) {
-            newPoints.push(point);
-            prevPoint = point;
-        }
+      if (point.marked || getSqDist(point, prevPoint) > sqTolerance) {
+        newPoints.push(point);
+        prevPoint = point;
+      }
     }
 
     if (prevPoint !== point) newPoints.push(point);
 
     return newPoints;
-}
+  }
 
-function simplifyDPStep(points, first, last, sqTolerance, simplified) {
+  function simplifyDPStep(points, first, last, sqTolerance, simplified) {
     var maxSqDist = sqTolerance;
     var index = -1;
-	var marked = false;
+    var marked = false;
     for (var i = first + 1; i < last; i++) {
-        var sqDist = getSqSegDist(points[i], points[first], points[last]);
+      var sqDist = getSqSegDist(points[i], points[first], points[last]);
 
-        if (sqDist > maxSqDist) {
-            index = i;
-            maxSqDist = sqDist;
-        }
-        /*if(points[i].marked && maxSqDist <= sqTolerance){
+      if (sqDist > maxSqDist) {
+        index = i;
+        maxSqDist = sqDist;
+      }
+      /*if(points[i].marked && maxSqDist <= sqTolerance){
         	index = i;
         	marked = true;
         }*/
     }
-    
+
     /*if(!points[index] && maxSqDist > sqTolerance){
     	console.log('shit shit shit');
     }*/
 
     if (maxSqDist > sqTolerance || marked) {
-        if (index - first > 1) simplifyDPStep(points, first, index, sqTolerance, simplified);
-        simplified.push(points[index]);
-        if (last - index > 1) simplifyDPStep(points, index, last, sqTolerance, simplified);
+      if (index - first > 1)
+        simplifyDPStep(points, first, index, sqTolerance, simplified);
+      simplified.push(points[index]);
+      if (last - index > 1)
+        simplifyDPStep(points, index, last, sqTolerance, simplified);
     }
-}
+  }
 
-// simplification using Ramer-Douglas-Peucker algorithm
-function simplifyDouglasPeucker(points, sqTolerance) {
+  // simplification using Ramer-Douglas-Peucker algorithm
+  function simplifyDouglasPeucker(points, sqTolerance) {
     var last = points.length - 1;
 
     var simplified = [points[0]];
@@ -106,11 +104,10 @@ function simplifyDouglasPeucker(points, sqTolerance) {
     simplified.push(points[last]);
 
     return simplified;
-}
+  }
 
-// both algorithms combined for awesome performance
-function simplify(points, tolerance, highestQuality) {
-
+  // both algorithms combined for awesome performance
+  function simplify(points, tolerance, highestQuality) {
     if (points.length <= 2) return points;
 
     var sqTolerance = tolerance !== undefined ? tolerance * tolerance : 1;
@@ -119,8 +116,7 @@ function simplify(points, tolerance, highestQuality) {
     points = simplifyDouglasPeucker(points, sqTolerance);
 
     return points;
-}
+  }
 
-window.simplify = simplify;
-
+  window.simplify = simplify;
 })();
