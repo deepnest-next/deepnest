784f79ff34212d6502bda2fef1e1c1356188a04b
Josef Fröhle <github@josef-froehle.de>
2024-12-23 13:03:04 +0100

    ✂️  remove internal calculateNFP to external module (#14)
    * remove calculateNFP source
    * use our external module @deepnest/calculate-nfp

--- main/background.js
+++ main/background.js
@@ -99,7 +99,7 @@ window.db = {
 window.onload = function () {
 	const { ipcRenderer } = require('electron');
 	window.ipcRenderer = ipcRenderer;
-	window.addon = require('../minkowski/Release/addon');
+	window.addon = require('@deepnest/calculate-nfp');
 	
 	window.path = require('path')
 	window.url = require('url')
