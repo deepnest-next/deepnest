ee757edf0929740e28717483cbd82f371ed28e45
Josef Fröhle <github@josef-froehle.de>
2024-12-27 11:33:05 +0100

    Changes For later
    - create own tsconfig for tests
    - format using settings
    - change new electron paths

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { devices, defineConfig } from "@playwright/test";
+import { devices, defineConfig } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -12,7 +12,7 @@ import { devices, defineConfig } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,31 +22,31 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { open: process.env.CI ? "never" : "on-failure" }]],
+  reporter: [['html', { open: process.env.CI ? 'never' : 'on-failure' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-    video: process.env.CI ? "retain-on-failure" : "on",
-    screenshot: "on",
-    headless: false,
+    trace: 'on-first-retry',
+    video: process.env.CI ? 'retain-on-failure' : 'on',
+    screenshot: 'on',
+    headless: false
   },
   metadata: {
-    pipeConsole: !process.env.CI,
+    pipeConsole: !process.env.CI
   },
 
-  snapshotPathTemplate: "{testDir}/{testFilePath}-snapshots/{arg}{ext}",
+  snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}{ext}',
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-  ],
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    }
+  ]
 
   /* Run your local dev server before starting the tests */
   // webServer: {
@@ -54,4 +54,4 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+})
