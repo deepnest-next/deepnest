80ae128206dad893978a11497463423a1f655662
Josef Fröhle <github@josef-froehle.de>
2025-01-13 13:33:22 +0100

    setup electron vite

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,13 +1,38 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin, bytecodePlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, swcPlugin } from 'electron-vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin(), bytecodePlugin()]
+    resolve: {
+      alias: {
+        '@shared': resolve('src/shared/src')
+      }
+    },
+    plugins: [
+      externalizeDepsPlugin({
+        include: ['@deepnest/clipper2', 'piscina']
+      }),
+      swcPlugin()
+    ],
+    build: {
+      rollupOptions: {
+        external: ['@deepnest/clipper2', 'piscina'],
+        input: {
+          index: resolve(__dirname, 'src/main/index.ts'),
+          'background.woker': resolve(__dirname, 'src/main/worker/index.ts')
+          //worker: resolve(__dirname, 'node_modules/piscina/src/worker.ts')
+        }
+      }
+    }
   },
   preload: {
-    plugins: [externalizeDepsPlugin(), bytecodePlugin()],
+    resolve: {
+      alias: {
+        '@shared': resolve('src/shared/src')
+      }
+    },
+    plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
         input: {
@@ -23,12 +48,12 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
+    optimizeDeps: {
+      exclude: ['@deepnest/clipper2']
+    },
     plugins: [solid()],
     build: {
       rollupOptions: {
-        output: {
-          format: 'cjs'
-        },
         input: {
           index: resolve(__dirname, 'src/renderer/index.html'),
           backend: resolve(__dirname, 'src/renderer/backend.html')
